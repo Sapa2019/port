0b88eca8b0c88ff5a17970a47fbfc363a7b61817
Involta <s.sapa@involta.ru>
2020-11-14 13:10:29 +0300

    Second Commit Admin

--- app/admin/index.php
+++ app/admin/index.php
@@ -6,6 +6,7 @@
 
  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
  	<link rel="stylesheet" href="css/styles.css">
+ 	<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"> -->
  </head>
  <body>
 
@@ -30,8 +31,8 @@
  			<h4>Sapa</h4>
  		</center>
 
- 		<a href="#"><i class="fas fa-desktop"></i><span></span></a>
- 		<a href="#"><i class="fas fa-cogs"></i><span>Components</span></a>
+ 		<a href="#"><i class="fas fa-desktop"></i><span>Desktop</span></a>
+ 		<a href="#"><i class="fas fa-cogs"></i><span>Menu</span></a>
  		<a href="#"><i class="fas fa-table"></i><span>Tables</span></a>
  		<a href="#"><i class="fas fa-th"></i><span>Forms</span></a>
  		<a href="#"><i class="fas fa-info-circle"></i><span>About</span></a>
@@ -39,7 +40,37 @@
  	</div>
 
  	<div class="content">
-
+ 		<div id="content_table" style="padding-top: 100px;padding-left: 200px;">
+ 			<table class="">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+ 		</div>
  	</div>
  
  </body>
--- app/http/controller.php
+++ app/http/controller.php
@@ -1,1 +1,7 @@
-controller.php
+<?php
+
+
+
+
+
+?>
